b41d7f63b19d53d1ba202b5205956f62c2915d6a
Alexey <alexey.tatarchuk@gmail.com>
2022-11-19 15:24:00 +0300

    Add quicksort

---
+++ Sorting/quicksort.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	arr := rand.Perm(10)
+	fmt.Println(arr)
+	fmt.Println(quickSort(arr, 0, len(arr)-1))
+}
+
+func quickSort(arr []int, low, high int) []int {
+	if low < high {
+		var p int
+		arr, p = partition(arr, low, high)
+		arr = quickSort(arr, low, p-1)
+		arr = quickSort(arr, p+1, high)
+	}
+	return arr
+}
+
+//Hoare partition scheme
+func partition(arr []int, low, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
+}
